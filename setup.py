96d7b346f20db3201f8e0d1f4ba6fbd184272036
John Wittenauer <jdwittenauer@gmail.com>
2015-08-22 15:17:56 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='ionyx',
+      version='0.0.1',
+      description='High-level machine learning experimentation library.',
+      author='John Wittenauer',
+      author_email='jdwittenauer@gmail.com',
+      url='https://github.com/jdwittenauer/ionyx',
+      license='Apache',
+      install_requires=[],
+      packages=find_packages())
