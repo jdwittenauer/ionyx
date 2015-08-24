87a934a141192cff61c16d2ad6b9555a6c23ee82
John Wittenauer <jdwittenauer@gmail.com>
2015-08-24 09:09:35 -0400

    Adding class placeholder files

---
+++ ionyx/transforms/all_label_encoder.py
@@ -0,0 +1,12 @@
+class AllLabelEncoder(object):
+    def __init__(self):
+        print('TODO')
+
+    def fit(self):
+        print('TODO')
+
+    def transform(self):
+        print('TODO')
+
+    def fit_transform(self):
+        print('TODO')
