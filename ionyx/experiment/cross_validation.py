c24fa1dd9900e2232a286c321d36d9d49fd95f19
John Wittenauer <jdwittenauer@gmail.com>
2015-09-03 22:10:06 -0400

    Still incorporating code from past competitions

--- ionyx/experiment/cross_validation.py
+++ ionyx/experiment/cross_validation.py
@@ -1,1 +1,123 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cross_validation import KFold
+from sklearn.learning_curve import learning_curve
 
+from ..utils.utils import fit_transforms, apply_transforms, score
+
+
+def cross_validate(X, y, algorithm, model, metric, transforms, n_folds):
+    """
+    Performs manual cross-validation to estimate the true performance of the model.
+    """
+    t0 = time.time()
+    y_pred = np.array([])
+    y_true = np.array([])
+
+    folds = list(KFold(y.shape[0], n_folds=n_folds, shuffle=True, random_state=1337))
+    for i, (train_index, eval_index) in enumerate(folds):
+        print('Starting fold {0}...'.format(i + 1))
+        X_train = X[train_index]
+        y_train = y[train_index]
+        X_eval = X[eval_index]
+        y_eval = y[eval_index]
+
+        transforms = fit_transforms(X_train, y_train, transforms)
+        X_train = apply_transforms(X_train, transforms)
+        X_eval = apply_transforms(X_eval, transforms)
+
+        if algorithm == 'nn':
+            model.fit(X_train, y_train, batch_size=128, nb_epoch=100, verbose=0,
+                      validation_data=(X_eval, y_eval), shuffle=True)
+        elif algorithm == 'xgb':
+            model.fit(X_train, y_train)
+
+        y_pred = np.append(y_pred, model.predict(X_eval))
+        y_true = np.append(y_true, y_eval)
+
+    t1 = time.time()
+    print('Cross-validation completed in {0:3f} s.'.format(t1 - t0))
+
+    cross_validation_score = score(y_true, y_pred, metric)
+    print('Cross-validation score ='), cross_validation_score
+
+    return cross_validation_score
+
+
+def sequence_cross_validate(X, y, model, metric, transforms, n_folds, strategy='traditional',
+                            window_type='cumulative', min_window=0, forecast_range=1, plot=False):
+    """
+    Performs time series cross-validation to estimate the true performance of the model.
+    """
+    scores = []
+    train_count = len(X)
+
+    if strategy == 'walk-forward':
+        n_folds = train_count - min_window - forecast_range
+        fold_size = 1
+    else:
+        fold_size = train_count / n_folds
+
+    t0 = time.time()
+    for i in range(n_folds):
+        if window_type == 'fixed':
+            fold_start = i * fold_size
+        else:
+            fold_start = 0
+
+        fold_end = (i + 1) * fold_size + min_window
+        fold_train_end = fold_end - forecast_range
+
+        X_train, X_eval = X[fold_start:fold_train_end, :], X[fold_train_end:fold_end, :]
+        y_train, y_eval = y[fold_start:fold_train_end], y[fold_train_end:fold_end]
+
+        transforms = fit_transforms(X_train, y_train, transforms)
+        X_train = apply_transforms(X_train, transforms)
+        X_eval = apply_transforms(X_eval, transforms)
+
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_eval)
+        scores.append(score(y, y_pred, metric))
+
+        if plot is True:
+            fig, ax = plt.subplots(figsize=(16, 10))
+            ax.set_title('Estimation Error')
+            ax.plot(y_pred - y_eval)
+            fig.tight_layout()
+
+    t1 = time.time()
+    print('Cross-validation completed in {0:3f} s.'.format(t1 - t0))
+
+    return np.mean(scores)
+
+
+def plot_learning_curve(X, y, model, metric, transforms, n_folds):
+    """
+    Plots a learning curve showing model performance against both training and
+    validation data sets as a function of the number of training samples.
+    """
+    transforms = fit_transforms(X, y, transforms)
+    X = apply_transforms(X, transforms)
+
+    t0 = time.time()
+    train_sizes, train_scores, test_scores = learning_curve(model, X, y, scoring=metric, cv=n_folds, n_jobs=1)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    fig, ax = plt.subplots(figsize=(16, 10))
+    ax.set_title('Learning Curve')
+    ax.set_xlabel('Training Examples')
+    ax.set_ylabel('Score')
+    ax.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std,
+                    alpha=0.1, color='b')
+    ax.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std,
+                    alpha=0.1, color='r')
+    ax.plot(train_sizes, train_scores_mean, 'o-', color='b', label='Training score')
+    ax.plot(train_sizes, test_scores_mean, 'o-', color='r', label='Cross-validation score')
+    ax.legend(loc='best')
+    fig.tight_layout()
+    t1 = time.time()
+    print('Learning curve generated in {0:3f} s.'.format(t1 - t0))
--- ionyx/experiment/model.py
+++ ionyx/experiment/model.py
@@ -1,11 +1,5 @@
 import time
-
-from sklearn.cross_validation import *
-from sklearn.ensemble import *
-from sklearn.linear_model import *
-from sklearn.naive_bayes import *
-from sklearn.svm import *
-
+from sklearn.cross_validation import train_test_split
 from xgboost import *
 from keras.callbacks import *
 from keras.layers.core import *
@@ -15,71 +9,6 @@ from keras.models import *
 from keras.optimizers import *
 
 from ..utils.utils import fit_transforms, apply_transforms, predict_score
-
-
-def define_model(model_type, algorithm):
-    """
-    Defines and returns a model object of the designated type.
-    """
-    model = None
-
-    if model_type == 'classification':
-        if algorithm == 'bayes':
-            model = GaussianNB()
-        elif algorithm == 'logistic':
-            model = LogisticRegression(penalty='l2', C=1.0)
-        elif algorithm == 'svm':
-            model = SVC(C=1.0, kernel='rbf', shrinking=True, probability=False, cache_size=200)
-        elif algorithm == 'sgd':
-            model = SGDClassifier(loss='hinge', penalty='l2', alpha=0.0001, n_iter=1000, shuffle=False, n_jobs=-1)
-        elif algorithm == 'forest':
-            model = RandomForestClassifier(n_estimators=10, criterion='gini', max_features='auto', max_depth=None,
-                                           min_samples_split=2, min_samples_leaf=1, max_leaf_nodes=None, n_jobs=-1)
-        elif algorithm == 'xt':
-            model = ExtraTreesClassifier(n_estimators=10, criterion='gini', max_features='auto', max_depth=None,
-                                         min_samples_split=2, min_samples_leaf=1, max_leaf_nodes=None, n_jobs=-1)
-        elif algorithm == 'boost':
-            model = GradientBoostingClassifier(loss='deviance', learning_rate=0.1, n_estimators=100, subsample=1.0,
-                                               min_samples_split=2, min_samples_leaf=1, max_depth=3, max_features=None,
-                                               max_leaf_nodes=None)
-        elif algorithm == 'xgb':
-            model = XGBClassifier(max_depth=3, learning_rate=0.1, n_estimators=100, silent=True,
-                                  objective='multi:softmax', gamma=0, min_child_weight=1, max_delta_step=0,
-                                  subsample=1.0, colsample_bytree=1.0, base_score=0.5, seed=0, missing=None)
-        else:
-            print('No model defined for ' + algorithm)
-            exit()
-    else:
-        if algorithm == 'ridge':
-            model = Ridge(alpha=1.0)
-        elif algorithm == 'svm':
-            model = SVR(C=1.0, kernel='rbf', shrinking=True, cache_size=200)
-        elif algorithm == 'sgd':
-            model = SGDRegressor(loss='squared_loss', penalty='l2', alpha=0.0001, n_iter=1000, shuffle=False)
-        elif algorithm == 'forest':
-            model = RandomForestRegressor(n_estimators=10, criterion='mse', max_features='auto', max_depth=None,
-                                          min_samples_split=2, min_samples_leaf=1, max_leaf_nodes=None, n_jobs=-1)
-        elif algorithm == 'xt':
-            model = ExtraTreesRegressor(n_estimators=10, criterion='mse', max_features='auto', max_depth=None,
-                                        min_samples_split=2, min_samples_leaf=1, max_leaf_nodes=None, n_jobs=-1)
-        elif algorithm == 'boost':
-            model = GradientBoostingRegressor(loss='ls', learning_rate=0.1, n_estimators=100, subsample=1.0,
-                                              min_samples_split=2, min_samples_leaf=1, max_depth=3, max_features=None,
-                                              max_leaf_nodes=None)
-        elif algorithm == 'xgb':
-            # model = XGBRegressor(max_depth=3, learning_rate=0.01, n_estimators=1000, silent=True,
-            #                      objective='reg:linear', gamma=0, min_child_weight=1, max_delta_step=0,
-            #                      subsample=1.0, colsample_bytree=1.0, base_score=0.5, seed=0, missing=None)
-            xg = XGBRegressor(max_depth=7, learning_rate=0.005, n_estimators=1800, silent=True,
-                              objective='reg:linear', gamma=0, min_child_weight=1, max_delta_step=0,
-                              subsample=0.9, colsample_bytree=0.8, base_score=0.5, seed=0, missing=None)
-            model = BaggingRegressor(base_estimator=xg, n_estimators=10, max_samples=1.0, max_features=1.0,
-                                     bootstrap=True, bootstrap_features=False)
-        else:
-            print('No model defined for ' + algorithm)
-            exit()
-
-    return model
 
 
 def define_nn_model(input_size, layer_size, output_size, n_hidden_layers, init_method, loss_function,
