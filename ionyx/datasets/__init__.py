ed24f9ef63703c0399877593ceb68a9a5b527cab
John Wittenauer <jdwittenauer@gmail.com>
2015-09-16 16:06:52 -0400

    Adding data sets

---
+++ ionyx/datasets/__init__.py
@@ -0,0 +1,4 @@
+from .datasets import load_bike_sharing
+from .datasets import load_forest_cover
+from .datasets import load_otto_group
+from .datasets import load_property_inspection
--- ionyx/datasets/datasets.py
+++ ionyx/datasets/datasets.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 from zipfile import ZipFile
+from sklearn.preprocessing import LabelEncoder
+
+from ..utils import CategoryEncoder
 
 
 def load_bike_sharing():
@@ -28,9 +31,8 @@ def load_forest_cover():
     data = data.set_index('Id')
 
     # move the label to the first position
-    num_features = len(data.columns) - 1
     cols = data.columns.tolist()
-    cols = cols[-1:] + cols[0:num_features]
+    cols = cols[-1:] + cols[0:-1]
     data = data[cols]
 
     X = data.iloc[:, 1:].values
@@ -42,12 +44,34 @@ def load_forest_cover():
 def load_otto_group():
     z = ZipFile(os.getcwd() + '/data/otto_group.zip')
     data = pd.read_csv(z.open('train.csv'))
+    data = data.set_index('id')
 
-    return data
+    # move the label to the first position
+    cols = data.columns.tolist()
+    cols = cols[-1:] + cols[0:-1]
+    data = data[cols]
+
+    X = data.iloc[:, 1:].values
+
+    y = data.iloc[:, 0].values
+
+    # transform the labels from strings to integers
+    encoder = LabelEncoder()
+    y = encoder.fit_transform(y)
+
+    return data, X, y
 
 
 def load_property_inspection():
     z = ZipFile(os.getcwd() + '/data/property_inspection.zip')
     data = pd.read_csv(z.open('train.csv'))
+    data = data.set_index('Id')
 
-    return data
+    X = data.iloc[:, 1:].values
+    y = data.iloc[:, 0].values
+
+    # transform the categorical variables from strings to integers
+    encoder = CategoryEncoder()
+    X = encoder.fit_transform(X)
+
+    return data, X, y
