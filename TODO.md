9e757deccd28f9767a5e16cda083de91ca4d937a
John Wittenauer <jdwittenauer@gmail.com>
2015-09-22 20:16:31 -0400

    Updated model training function

--- TODO.md
+++ TODO.md
@@ -5,7 +5,7 @@ This project is extremely early in development.  Below is a summary of tasks req
 
 - implement a high-level "experiment" class to tie everything together (design still needs fleshed out)
 - add unit tests for each class/function
-- add sample data sets with example code
+- add example code using the sample data sets
 - add detailed doc strings for each class/function
 - add project documentation and usage examples
 - re-factor visualization functions to have a cleaner API
@@ -13,5 +13,5 @@ This project is extremely early in development.  Below is a summary of tasks req
 - create emsemble classes and make the design more flexible than current functions
 - update model definition, training, cross-validation, and parameter search methods to be more generic
 - extend support for loading and saving models to handle a much wider variety of use cases
-- extend parameter search capabilities to be flexible and use random search where possible
-- add optional extensions/dependencies for additional libraries
+- model.py - XGBoost min eval loss/iteration
+- model.py - training history plot function
--- ionyx/experiment/model.py
+++ ionyx/experiment/model.py
@@ -1,31 +1,80 @@
 import time
-from xgboost import *
-from keras.callbacks import *
-from keras.layers.core import *
-from keras.layers.normalization import *
-from keras.layers.advanced_activations import *
-from keras.models import *
-from keras.optimizers import *
+from sklearn.cross_validation import train_test_split
 
 from ..utils import fit_transforms, apply_transforms, predict_score
 
 
-def train_model(X, y, model, metric, transforms):
+def train_model(X, y, model, package, metric, transforms, eval=False, plot_eval_history=False,
+                early_stopping=False, early_stopping_rounds=None):
     """
     Trains a new model using the training data.
     """
     t0 = time.time()
-    transforms = fit_transforms(X, y, transforms)
-    X = apply_transforms(X, transforms)
-    model.fit(X, y)
+    X_train = None
+    X_eval = None
+    y_train = None
+    y_eval = None
+    training_history = None
+
+    if eval:
+        X_train, X_eval, y_train, y_eval = train_test_split(X, y, test_size=0.1)
+        transforms = fit_transforms(X_train, y_train, transforms)
+        X_train = apply_transforms(X_train, transforms)
+        X_eval = apply_transforms(X_eval, transforms)
+
+        if early_stopping:
+            if package == 'xgboost':
+                model.fit(X_train, y_train, eval_set=[(X_eval, y_eval)], eval_metric='rmse',
+                          early_stopping_rounds=early_stopping_rounds)
+                training_history = model.eval_results
+                print('Best iteration found ='), model.best_iteration
+            else:
+                raise Exception('Early stopping not supported.')
+        else:
+            if package == 'xgboost':
+                model.fit(X_train, y_train, eval_set=[(X_eval, y_eval)], eval_metric='rmse')
+                training_history = model.eval_results
+                print('TODO')
+            elif package == 'keras':
+                model.validation_data = (X_eval, y_eval)
+                training_history = model.fit(X_train, y_train)
+                print('Min eval loss ='), min(training_history.history['val_loss'])
+                print('Min eval epoch ='), min(enumerate(training_history.history['loss']), key=lambda x: x[1])[0] + 1
+            else:
+                raise Exception('Model evaluation not supported.')
+    else:
+        transforms = fit_transforms(X, y, transforms)
+        X = apply_transforms(X, transforms)
+        if package == 'keras':
+            training_history = model.fit(X, y)
+        else:
+            model.fit(X, y)
+
     t1 = time.time()
     print('Model trained in {0:3f} s.'.format(t1 - t0))
 
     print('Model hyper-parameters:')
     print(model.get_params())
 
-    print('Calculating training score...')
-    model_score = predict_score(X, y, model, metric)
-    print('Training score ='), model_score
+    if eval:
+        print('Calculating training score...')
+        train_score = predict_score(X_train, y_train, model, metric)
+        print('Training score ='), train_score
 
-    return model
+        print('Calculating evaluation score...')
+        eval_score = predict_score(X_eval, y_eval, model, metric)
+        print('Evaluation score ='), eval_score
+
+        if plot_eval_history:
+            if package == 'xgboost':
+                print('TODO')
+            elif package == 'keras':
+                print('TODO')
+            else:
+                raise Exception('Eval history not supported.')
+    else:
+        print('Calculating training score...')
+        train_score = predict_score(X, y, model, metric)
+        print('Training score ='), train_score
+
+    return model, training_history
