c1bf3754085bae49b022939205b56204526d7ea7
John Wittenauer <jdwittenauer@gmail.com>
2016-05-26 20:21:12 -0400

    Minor bug fix

--- ionyx/experiment/__init__.py
+++ ionyx/experiment/__init__.py
@@ -2,6 +2,4 @@ from .cross_validation import cross_validate
 from .cross_validation import sequence_cross_validate
 from .cross_validation import plot_learning_curve
 from .model import train_model
-from .param_search import parameter_search
-from .param_search import xbg_parameter_search
-from .param_search import nn_parameter_search
+from .param_search import parameter_grid_search
