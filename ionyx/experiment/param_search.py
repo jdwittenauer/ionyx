427175e5db138b9877ea6dbdf7a9199b43b2cbc2
John Wittenauer <jdwittenauer@gmail.com>
2016-11-04 20:05:53 -0400

    Working on documentation

--- ionyx/experiment/param_search.py
+++ ionyx/experiment/param_search.py
@@ -8,6 +8,30 @@ from ..utils import fit_transforms, apply_transforms, predict_score
 def parameter_grid_search(X, y, model, metric, transform_grid, param_grid, test_split_size=0.2):
     """
     Performs an exhaustive search over the specified model parameters.
+
+    Parameters
+    ----------
+    X : array-like
+        Training input samples.
+
+    y : array-like
+        Target values.
+
+    model : object
+        An object in memory that represents a model definition.
+
+    metric : {'accuracy', 'f1', 'log_loss', 'mean_absolute_error', 'mean_squared_error', 'r2', 'roc_auc'}
+        Scoring metric.
+
+    transform_grid : array-like
+        List of lists of transforms to experiment with.  The function will try each combination
+        of transforms for every parameter combination specified in param_grid.
+
+    param_grid : array-like
+        List of dictionaries containing the parameter/value combinations to iterate over.
+
+    test_split_size : float, optional, default 0.2
+        Proportion of the data to hold out for evaluation (range 0 to 1).
     """
     print('Beginning parameter grid search...')
     t0 = time.time()
