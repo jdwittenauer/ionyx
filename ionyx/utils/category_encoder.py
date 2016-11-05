7bc70cd0c133217be63fe5c7bb31ea1e8b5deecd
John Wittenauer <jdwittenauer@gmail.com>
2016-11-05 10:21:07 -0400

    Working on documentation

--- ionyx/utils/category_encoder.py
+++ ionyx/utils/category_encoder.py
@@ -3,11 +3,29 @@ from sklearn.preprocessing import LabelEncoder
 
 
 class CategoryEncoder(object):
+    """
+    Transform class that encodes text-based categorical variables into integers ranging from 0 to
+    distinct_values-1, where distinct_values is the number of unique values in each category.
+
+    Parameters
+    ----------
+    categorical_features : array-like, optional, default None
+        A list of integers representing the column indices to apply the transform to.  If None,
+        the transform will attempt to apply itself to all columns with string values.
+    """
     def __init__(self, categorical_features=None):
         self.categorical_features = categorical_features
         self.encoders_ = {}
 
     def fit(self, X):
+        """
+        Fit the transform using X as the training data.
+
+        Parameters
+        ----------
+        X : array-like
+            Training input samples.
+        """
         if self.categorical_features is not None:
             for i in self.categorical_features:
                 encoder = LabelEncoder()
@@ -21,6 +39,14 @@ class CategoryEncoder(object):
                     self.encoders_[i] = encoder
 
     def transform(self, X):
+        """
+        Apply the transform to the data.
+
+        Parameters
+        ----------
+        X : array-like
+            Training input samples.
+        """
         X_trans = np.copy(X)
         for index, encoder in self.encoders_.iteritems():
             X_trans[:, index] = encoder.transform(X[:, index])
@@ -28,8 +54,19 @@ class CategoryEncoder(object):
         return X_trans
 
     def fit_transform(self, X):
+        """
+        Wrapper method that calls fit and transform sequentially.
+
+        Parameters
+        ----------
+        X : array-like
+            Training input samples.
+        """
         self.fit(X)
         return self.transform(X)
 
     def __repr__(self):
+        """
+        Overrides the method that prints a string representation of the object.
+        """
         return '%s' % self.__class__.__name__
--- ionyx/utils/category_to_numeric.py
+++ ionyx/utils/category_to_numeric.py
@@ -2,12 +2,37 @@ import numpy as np
 
 
 class CategoryToNumeric(object):
-    def __init__(self, categorical_features=None, metric='mean'):
+    """
+    Transform class that replaces a categorical value with a representative target value for instances
+    that belong to that category.  This technique is useful as a method to turn categorical features
+    into numeric values for use in an estimator, and can be viewed as an alternative approach to
+    one-hot encoding.  Only suitable for regression tasks.
+
+    Parameters
+    ----------
+    categorical_features : array-like
+        A list of integers representing the column indices to apply the transform to.
+
+    metric : {'mean', 'median', 'std'}, optional, default 'mean'
+        The method used to calculate the replacement value for a category.
+    """
+    def __init__(self, categorical_features, metric='mean'):
         self.categorical_features = categorical_features
         self.metric = metric
         self.feature_map_ = {}
 
     def fit(self, X, y):
+        """
+        Fit the transform using X as the training data and y as the label.
+
+        Parameters
+        ----------
+        X : array-like
+            Training input samples.
+
+        y : array-like
+            Target values.
+        """
         for i in self.categorical_features:
             self.feature_map_[i] = {}
             distinct = list(np.unique(X[:, i]))
@@ -22,6 +47,14 @@ class CategoryToNumeric(object):
                     raise Exception('Metric not not recognized.')
 
     def transform(self, X):
+        """
+        Apply the transform to the data.
+
+        Parameters
+        ----------
+        X : array-like
+            Training input samples.
+        """
         X_trans = np.copy(X)
         for i in self.categorical_features:
             distinct = list(np.unique(X_trans[:, i]))
@@ -31,8 +64,22 @@ class CategoryToNumeric(object):
         return X_trans
 
     def fit_transform(self, X, y):
+        """
+        Wrapper method that calls fit and transform sequentially.
+
+        Parameters
+        ----------
+        X : array-like
+            Training input samples.
+
+        y : array-like
+            Target values.
+        """
         self.fit(X, y)
         return self.transform(X)
 
     def __repr__(self):
+        """
+        Overrides the method that prints a string representation of the object.
+        """
         return '%s' % self.__class__.__name__
