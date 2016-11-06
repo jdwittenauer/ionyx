979f2901d6b3179db774999e86ba32a599d9daca
John Wittenauer <jdwittenauer@gmail.com>
2016-11-06 10:40:59 -0500

    Working on documentation

--- ionyx/utils/utils.py
+++ ionyx/utils/utils.py
@@ -9,27 +9,56 @@ def create_class(import_path, module_name, class_name, *params):
 
     Parameters
     ----------
-    import_path: The path to the module
-    module_name: The module name
-    class_name: The class name
-    params: Any fields required to instantiate the class
+    import_path : string
+        The path to the module.
+
+    module_name : string
+        The module name.
+
+    class_name : string
+        The class name.
+
+    params : array-like
+        Any fields required to instantiate the class.
 
     Returns
     ----------
-    self: An instance of the class.
+    instance : object
+        An instance of the class.
     """
     p = __import__(import_path)
     m = getattr(p, module_name)
     c = getattr(m, class_name)
     instance = c(*params)
+
     return instance
 
 
-def load_csv_data(directory, filename, dtype=None, index=None, convert_to_date=False):
+def load_csv_data(filename, dtype=None, index=None, convert_to_date=False):
     """
-    Load a csv data file into a data frame, setting the index as appropriate.
+    Load a csv data file into a data frame.  This function wraps Pandas' read_csv function with logic to set the
+    index for the data frame and convert fields to date types if necessary.
+
+    Parameters
+    ----------
+    filename : string
+        Location of the file to read.
+
+    dtype : array-like, optional, default None
+        List of column types to set for the data frame.
+
+    index : string, array-like, optional, default None
+        String or list of strings specifying the columns to use as an index.
+
+    convert_to_date : boolean, optional, default False
+        Boolean indicating if the index consists of date fields.
+
+    Returns
+    ----------
+    data : array-like
+        Data frame containing data from the input file.
     """
-    data = pd.read_csv(directory + filename, sep=',', dtype=dtype)
+    data = pd.read_csv(filename, sep=',', dtype=dtype)
 
     if index is not None:
         if convert_to_date:
@@ -45,9 +74,20 @@ def load_csv_data(directory, filename, dtype=None, index=None, convert_to_date=F
 
     return data
 
+
 def load_model(filename):
     """
-    Load a previously training model from disk.
+    Load a previously trained model from disk.
+
+    Parameters
+    ----------
+    filename : string
+        Location of the file to read.
+
+    Returns
+    ----------
+    model : object
+        An object in memory that represents a fitted model.
     """
     model_file = open(filename, 'rb')
     model = pickle.load(model_file)
@@ -59,6 +99,14 @@ def load_model(filename):
 def save_model(model, filename):
     """
     Persist a trained model to disk.
+
+    Parameters
+    ----------
+    model : object
+        An object in memory that represents a fitted model.
+
+    filename : string
+        Location of the file to write.
     """
     model_file = open(filename, 'wb')
     pickle.dump(model, model_file)
@@ -68,6 +116,22 @@ def save_model(model, filename):
 def fit_transforms(X, y, transforms):
     """
     Fits new transformations from a data set.
+
+    Parameters
+    ----------
+    X : array-like
+        Training input samples.
+
+    y : array-like
+        Target values.
+
+    transforms : array-like
+        List of objects with a fit_transform function that accepts two parameters.
+
+    Returns
+    ----------
+    transforms : array-like
+        List of transform objects after calling fit_transform on the input data.
     """
     for i, trans in enumerate(transforms):
         if trans is not None:
@@ -80,6 +144,19 @@ def fit_transforms(X, y, transforms):
 def apply_transforms(X, transforms):
     """
     Applies pre-computed transformations to a data set.
+
+    Parameters
+    ----------
+    X : array-like
+        Training input samples.
+
+    transforms : array-like
+        List of objects with a transform function that accepts one parameter.
+
+    Returns
+    ----------
+    X : array-like
+        Training input samples after iteratively applying each transform to the data.
     """
     for trans in transforms:
         if trans is not None:
@@ -91,7 +168,24 @@ def apply_transforms(X, transforms):
 def score(y, y_pred, metric):
     """
     Calculates a score for the given predictions using the provided metric.
+
+    Parameters
+    ----------
+    y : array-like
+        Target values.
+
+    y_pred : array-like
+        Predicted target values.
+
+    metric : {'accuracy', 'f1', 'log_loss', 'mean_absolute_error', 'mean_squared_error', 'r2', 'roc_auc'}
+        Scoring metric.
+
+    Returns
+    ----------
+    score : float
+        Calculated score between the target and predicted target values.
     """
+    y = y.ravel()
     y_pred = y_pred.ravel()
     assert y.shape == y_pred.shape
 
@@ -114,6 +208,25 @@ def score(y, y_pred, metric):
 def predict_score(X, y, model, metric):
     """
     Predicts and scores the model's performance and returns the result.
+
+    Parameters
+    ----------
+    X : array-like
+        Training input samples.
+
+    y : array-like
+        Target values.
+
+    model : object
+        An object in memory that represents a fitted model.
+
+    metric : {'accuracy', 'f1', 'log_loss', 'mean_absolute_error', 'mean_squared_error', 'r2', 'roc_auc', None}
+        Scoring metric.
+
+    Returns
+    ----------
+    score : float
+        Calculated score between the target and predicted target values.
     """
     if metric is not None:
         y_pred = model.predict(X)
