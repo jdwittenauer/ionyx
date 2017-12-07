94ee06017c4fefc10a315c2378fb53d0e0d89019
John Wittenauer <jdwittenauer@gmail.com>
2017-12-06 21:03:00 -0500

    Added improved cross-validation for time series.

--- ionyx/contrib/time_series_split.py
+++ ionyx/contrib/time_series_split.py
@@ -16,6 +16,7 @@ class TimeSeriesSplit(_BaseKFold):
     Note that unlike standard cross-validation methods, successive
     training sets are supersets of those that come before them.
     Read more in the :ref:`User Guide <cross_validation>`.
+
     Parameters
     ----------
     n_splits : int, default=3
@@ -24,21 +25,7 @@ class TimeSeriesSplit(_BaseKFold):
         Minimum size for a single training set.
     max_train_size : int, optional
         Maximum size for a single training set.
-    Examples
-    --------
-    # >>> from sklearn.model_selection import TimeSeriesSplit
-    # >>> X = np.array([[1, 2], [3, 4], [1, 2], [3, 4]])
-    # >>> y = np.array([1, 2, 3, 4])
-    # >>> tscv = TimeSeriesSplit(n_splits=3)
-    # >>> print(tscv)  # doctest: +NORMALIZE_WHITESPACE
-    # TimeSeriesSplit(max_train_size=None, n_splits=3)
-    # >>> for train_index, test_index in tscv.split(X):
-    # ...    print("TRAIN:", train_index, "TEST:", test_index)
-    # ...    X_train, X_test = X[train_index], X[test_index]
-    # ...    y_train, y_test = y[train_index], y[test_index]
-    TRAIN: [0] TEST: [1]
-    TRAIN: [0 1] TEST: [2]
-    TRAIN: [0 1 2] TEST: [3]
+
     Notes
     -----
     The training set has size ``i * n_samples // (n_splits + 1)
@@ -46,15 +33,16 @@ class TimeSeriesSplit(_BaseKFold):
     with a test set of size ``n_samples//(n_splits + 1)``,
     where ``n_samples`` is the number of samples.
     """
-    def __init__(self, n_splits=3, min_train_size=None, max_train_size=None):
+    def __init__(self, n_splits=3, max_train_size=None, min_train_size=None):
         super(TimeSeriesSplit, self).__init__(n_splits,
                                               shuffle=False,
                                               random_state=None)
-        self.min_train_size = min_train_size
         self.max_train_size = max_train_size
+        self.min_train_size = min_train_size
 
     def split(self, X, y=None, groups=None):
         """Generate indices to split data into training and test set.
+
         Parameters
         ----------
         X : array-like, shape (n_samples, n_features)
@@ -64,12 +52,14 @@ class TimeSeriesSplit(_BaseKFold):
             Always ignored, exists for compatibility.
         groups : array-like, with shape (n_samples,), optional
             Always ignored, exists for compatibility.
+
         Returns
         -------
         train : ndarray
             The training set indices for that split.
         test : ndarray
             The testing set indices for that split.
+
         Notes
         -----
         Randomized CV splitters may return different results for each call of
@@ -85,10 +75,22 @@ class TimeSeriesSplit(_BaseKFold):
                 ("Cannot have number of folds ={0} greater"
                  " than the number of samples: {1}.").format(n_folds,
                                                              n_samples))
+        if (self.min_train_size and self.max_train_size
+                and self.min_train_size > self.max_train_size):
+            raise ValueError(
+                ("Cannot have min_train_size ={0} greater"
+                 " than max_train_size: {1}").format(self.min_train_size,
+                                                     self.max_train_size))
         indices = np.arange(n_samples)
-        test_size = (n_samples // n_folds)
-        test_starts = range(test_size + n_samples % n_folds,
-                            n_samples, test_size)
+        if self.min_train_size:
+            test_size = ((n_samples - self.min_train_size) // n_splits)
+            test_starts = range(self.min_train_size +
+                                (n_samples - self.min_train_size) % n_splits,
+                                n_samples, test_size)
+        else:
+            test_size = (n_samples // n_folds)
+            test_starts = range(test_size + n_samples % n_folds,
+                                n_samples, test_size)
         for test_start in test_starts:
             if self.max_train_size and self.max_train_size < test_start:
                 yield (indices[test_start - self.max_train_size:test_start],
---
+++ ionyx/contrib/time_series_split_test.py
@@ -0,0 +1,25 @@
+import numpy as np
+from ionyx.contrib.time_series_split import TimeSeriesSplit
+
+X = np.array([[1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4]])
+y = np.array([0, 1, 2, 3, 4, 5, 6, 7])
+
+tscv = TimeSeriesSplit(n_splits=3)
+print(tscv)
+for train_index, test_index in tscv.split(X):
+    print("TRAIN:", train_index, "TEST:", test_index)
+
+tscv = TimeSeriesSplit(n_splits=3, min_train_size=4)
+print(tscv)
+for train_index, test_index in tscv.split(X):
+    print("TRAIN:", train_index, "TEST:", test_index)
+
+tscv = TimeSeriesSplit(n_splits=3, max_train_size=4)
+print(tscv)
+for train_index, test_index in tscv.split(X):
+    print("TRAIN:", train_index, "TEST:", test_index)
+
+tscv = TimeSeriesSplit(n_splits=3, max_train_size=4, min_train_size=4)
+print(tscv)
+for train_index, test_index in tscv.split(X):
+    print("TRAIN:", train_index, "TEST:", test_index)
