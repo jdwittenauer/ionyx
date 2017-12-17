c9e9addd7a23efc36d91f1ef8b9cdbd9b86fe37d
John Wittenauer <jdwittenauer@gmail.com>
2017-12-17 14:54:05 -0500

    Adding test cases

---
+++ tests/category_encoder_test.py
@@ -0,0 +1,10 @@
+import numpy as np
+from ionyx.contrib import CategoryEncoder
+
+X = np.array([['a', 'x', 1], ['b', 'x', 2], ['c', 'y', 3], ['a', 'y', 4]])
+encoder = CategoryEncoder(categorical_features=[0, 1])
+X_trans = encoder.fit_transform(X)
+print('X:')
+print(X)
+print('X_trans:')
+print(X_trans)
---
+++ tests/category_to_numeric_test.py
@@ -0,0 +1,12 @@
+import numpy as np
+from ionyx.contrib import CategoryToNumeric
+
+X = np.array([[1.], [1.], [1.], [1.], [2.], [2.], [2.], [2.]])
+y = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+encoder = CategoryToNumeric(categorical_features=[0])
+X_trans = encoder.fit_transform(X, y)
+print('X:')
+print(X)
+print('X_trans:')
+print(X_trans)
