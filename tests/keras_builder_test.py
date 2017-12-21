3be1b5d014e4c09ea2c3d43c20077a10980122ce
John Wittenauer <jdwittenauer@gmail.com>
2017-12-21 13:32:10 -0500

    Still working on test cases

---
+++ tests/keras_builder_test.py
@@ -0,0 +1,20 @@
+import numpy as np
+from keras.utils.np_utils import to_categorical
+from keras.wrappers.scikit_learn import KerasClassifier
+from ionyx.contrib import KerasBuilder
+from ionyx.datasets import DataSetLoader
+
+data, X, y = DataSetLoader.load_forest_cover()
+n_classes = len(np.unique(y)) + 1
+
+model = KerasBuilder.build_dense_model(input_size=X.shape[1], output_size=n_classes,
+                                       loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(X, to_categorical(y, n_classes))
+score = model.evaluate(X, to_categorical(y, n_classes))
+print('Model score = {0}'.format(score[1]))
+
+estimator = KerasClassifier(build_fn=KerasBuilder.build_dense_model, input_size=X.shape[1],
+                            output_size=n_classes, loss='categorical_crossentropy', metrics=['accuracy'])
+estimator.fit(X, to_categorical(y, n_classes))
+score = estimator.score(X, to_categorical(y, n_classes))
+print('Estimator score = {0}'.format(score))
