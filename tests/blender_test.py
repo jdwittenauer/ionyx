a6411d717c455b50e80c84b074d20aa34ed7a392
John Wittenauer <jdwittenauer@gmail.com>
2018-01-18 21:13:53 -0500

    Finished blender testing

--- tests/blender_test.py
+++ tests/blender_test.py
@@ -17,4 +17,24 @@ blender = Blender(models=models, scoring_metric='neg_mean_squared_error')
 blender.build_ensemble(X, y, cv, retrain=True)
 print(blender.ensemble_)
 
+_, X, y = DataSetLoader.load_otto_group()
+X, y = shuffle(X, y, n_samples=10000)
+X = X[:, :30]
+models = [('logistic', LogisticRegression()), ('svc', SVC()), ('decision_tree', DecisionTreeClassifier()),
+          ('stacker', LogisticRegression())]
+layer_mask = [0, 0, 0, 1]
+cv = KFold()
+layer_cv = KFold()
+blender = Blender(models=models, scoring_metric='neg_mean_squared_error')
+blender.build_stacking_ensemble(X, y, cv, layer_cv, layer_mask, retrain=True)
+print(blender.ensemble_)
+
+_, X, y = DataSetLoader.load_otto_group()
+X, y = shuffle(X, y, n_samples=10000)
+X = X[:, :30]
+models = [('logistic', LogisticRegression()), ('svc', SVC()), ('decision_tree', DecisionTreeClassifier())]
+cv = KFold()
+blender = Blender(models=models, scoring_metric='neg_mean_squared_error')
+blender.model_correlations(X, y, cv)
+
 print('Done.')
