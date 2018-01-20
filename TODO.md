8e7c058a657109d811294899a6f7472240383289
John Wittenauer <jdwittenauer@gmail.com>
2018-01-20 11:00:46 -0500

    Finished work on optimizer

--- TODO.md
+++ TODO.md
@@ -3,5 +3,3 @@
 - Write detailed readme file
 - Look into documentation website
 - Work on some example notebooks
-- Optimizer ensemble method
-- Finish blender code/tests
--- tests/optimizer_test.py
+++ tests/optimizer_test.py
@@ -1,12 +1,14 @@
+import numpy as np
 from sklearn.model_selection import KFold
+from sklearn.utils import shuffle
 from ionyx import Optimizer
 from ionyx.datasets import DataSetLoader
 
 print('Beginning experiment test...')
 
 _, X, y = DataSetLoader.load_forest_cover()
-X = X[:5000, :20]
-y = y[:5000]
+X, y = shuffle(X, y, n_samples=5000)
+X = X[:, :20].astype(np.float64)
 cv = KFold()
 opt = Optimizer(X, y, cv, algorithm='bayes', scoring_metric='accuracy',
                 task='classification', search_type='grid')
@@ -14,12 +16,22 @@ pipe = opt.run()
 print(pipe)
 
 _, X, y = DataSetLoader.load_property_inspection()
-X = X[:5000, :20]
-y = y[:5000]
+X, y = shuffle(X, y, n_samples=5000)
+X = X[:, :20].astype(np.float64)
 cv = KFold()
 opt = Optimizer(X, y, cv, algorithm='ridge', scoring_metric='neg_mean_squared_error',
                 task='regression', search_type='random', n_iter=10)
 pipe = opt.run()
 print(pipe)
+
+_, X, y = DataSetLoader.load_forest_cover()
+X, y = shuffle(X, y, n_samples=5000)
+X = X[:, :20].astype(np.float64)
+cv = KFold()
+algorithms = ['bayes', 'logistic', 'random_forest']
+opt = Optimizer(X, y, cv, algorithm=algorithms, scoring_metric='accuracy',
+                task='classification', search_type='random', n_iter=10)
+ensemble = opt.run()
+print(ensemble)
 
 print('Done.')
